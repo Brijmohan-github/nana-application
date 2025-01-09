04a67a139c8889ff28c140447fcd10d033939b15
bmkaradia <bmk302006@gmail.com>
2025-01-09 18:35:23 +0530

    added vercelBlobStorage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -34,8 +35,21 @@ export default buildConfig({
     url: process.env.DATABASE_URI || '',
   }),
   sharp,
+  // plugins: [
+  //   payloadCloudPlugin(),
+  //   // storage-adapter-placeholder
+  // ],
   plugins: [
     payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true, // Optional, defaults to true
+      // Specify which collections should use Vercel Blob
+      collections: {
+        media: true,
+        // 'media-with-prefix': { prefix: 'my-prefix', },
+      },
+      // Token provided by Vercel once Blob storage is added to your Vercel project
+      token: process.env.BLOB_READ_WRITE_TOKEN!,
+    }),
   ],
 })
