75f11129775a02fc24aadde9f8c3bb66fccda7c5
bmkaradia <bmk302006@gmail.com>
2025-02-06 21:08:16 +0530

    </Suspense>

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import './globals.scss'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +11,9 @@ const inter = Inter({
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html className={inter.className}>
-      <body>{children}</body>
+      <body>
+        <Suspense>{children}</Suspense>
+      </body>
     </html>
   )
 }
