5ea78613fa00cc3ee038b5e918dce24c2619954a
imran khan <imran.khan.joya@gmail.com>
2025-02-22 10:23:14 +0530

    Update hasPermission.tsx

--- src/access/hasPermission.tsx
+++ src/access/hasPermission.tsx
@@ -1,22 +1,23 @@
 import { Access } from 'payload'
 
 export const hasPermission: Access = async ({ req }) => {
+  return true
   if (!req.user) {
     console.log('🚀 Brij ~ No user found - Access denied')
     return false
   }
 
-  const userRole = req.user.role || 'customer' // Assume customer if role is missing
+  // const userRole = req.user.role || 'customer' // Assume customer if role is missing
 
-  if (userRole === 'admin') {
-    console.log('🚀 Brij ~ Admin/Warehouse Role - Full Access')
-    return true // Full access for admins and warehouse
-  }
+  // if (userRole === 'admin') {
+  //   console.log('🚀 Brij ~ Admin/Warehouse Role - Full Access')
+  //   return true // Full access for admins and warehouse
+  // }
 
-  if (userRole === 'warehouse') {
-    console.log('🚀 Brij ~ Warehouse Role - no Access')
-    return false // Full access for admins and warehouse
-  }
+  // if (userRole === 'warehouse') {
+  //   console.log('🚀 Brij ~ Warehouse Role - no Access')
+  //   return false // Full access for admins and warehouse
+  // }
 
   return true
 
