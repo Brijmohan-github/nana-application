d4566f3fb566bc2366002a11a0efca00eb84ffc7
imran khan <imran.khan.joya@gmail.com>
2025-02-23 12:19:32 +0530

    access files

---
+++ src/access/isRead.tsx
@@ -0,0 +1,19 @@
+import { Access } from 'payload'
+
+
+export const isRead: Access = ({ req: { user } }) => {
+  const role = user?.role
+  if (role === 'admin') {
+    return true
+  } if (role === 'editor' || role === 'brand' || role === 'agency') {
+
+    return {
+      createdBy: {
+        equals: user?.id, // Only allow access to their own data
+      },
+    }
+  } 
+  else {
+    return false
+  }
+}
---
+++ src/access/isReadUser.tsx
@@ -0,0 +1,19 @@
+import { Access } from 'payload'
+
+
+export const isReadUser: Access = ({ req: { user } }) => {
+  const role = user?.role
+  if (role === 'admin') {
+    return true
+  } if (role === 'editor' || role === 'brand' || role === 'agency') {
+
+    return {
+      id: {
+        equals: user?.id, // Only allow access to their own data
+      },
+    }
+  } 
+  else {
+    return false
+  }
+}
